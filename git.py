ab6d8e3ba27fbad388d19f790710928464d9f516
Kevin Ostheimer <info@impulsleistung.de>
2020-03-24 21:43:19 +0100

    daily routine

--- git.py
+++ git.py
@@ -50,3 +50,63 @@ git show 0da2e #first few characters of hash
 # view details of the second recend commit
 git show HEAD~1
 
+# remove unwanted files. Warning: Unwanted = files not tracked
+git status    # lists files not added
+git clean -n   # lists files that would be removed
+git clean -f   # finally removes files listed previously
+
+# Change GIT configuration
+# Let's get the config first
+git config --list --global
+	user.email=repl@datacamp.com
+	user.name=Rep Loop
+	core.editor=nano
+# Then set new K,V pairs
+git config --global user.email rep.loop@datacamp.com
+
+# Selective staging
+git add ./data/northern.csv    # We only want this one
+git commit -m "Adding data from northern region."    # The general commit will do
+	[master 42efe70] Adding data from northern region.
+	 1 file changed, 1 insertion(+)    # Only one affected
+	 
+# undo changes which are not stages yet
+# Changes not staged for commit:
+  # (use "git add <file>..." to update what will be committed)
+  # (use "git checkout -- <file>..." to discard changes in working directory)
+
+        # modified:   data/northern.csv
+
+git checkout -- ./data/northern.csv
+
+
+# Undo changes to a stages file with the reset-checkout combination
+git reset ./data/northern.csv
+# Unstaged changes after reset:
+# M       data/northern.csv
+git checkout -- ./data/northern.csv
+
+# Restore older version of file
+# first, get the version history of the file with the hashes
+git log -2 ./data/western.csv
+	commit 81f8995a9fbb0cc9fddc295526a40639371269f8
+	Author: Rep Loop <repl@datacamp.com>
+	Date:   Wed Feb 5 16:37:55 2020 +0000
+
+		Adding fresh data for western region.
+
+	commit 5be6e52827e7d9b398c3701486342aa3f7ae4b58
+	Author: Rep Loop <repl@datacamp.com>
+	Date:   Wed Feb 5 16:37:55 2020 +0000
+
+    Adding fresh data for southern and western regions.
+
+# second, checkout the version that is wanted to be the latest
+git checkout 5be6 data/western.csv
+# third, make sure that this is the correct version of the file
+cat data/western.csv
+# fourth, commit the file from stage to repository
+git commit data/western.csv -m "Using the older version"
+	[master efb22d3] Using the older version
+	 1 file changed, 3 deletions(-)
+
