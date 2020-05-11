ec8f78b1285dabbca7282aacab73517bc7c9fdd8
Kevin Ostheimer <info@impulsleistung.de>
2020-05-11 16:11:54 +0200

    Update py2.py

--- .preCode/py2.py
+++ .preCode/py2.py
@@ -873,3 +873,58 @@ plt.show()
 
 #################################################
 
+# Permutation replicate function computes an array of statistical solutions
+def draw_perm_reps(data_1, data_2, func, size=1):
+    """Generate multiple permutation replicates."""
+
+    # Initialize array of replicates: perm_replicates
+    perm_replicates = np.empty(size)
+
+    for i in range(size):
+        # Generate permutation sample
+        perm_sample_1, perm_sample_2 = permutation_sample(data_1, data_2)
+
+        # Compute the test statistic
+        perm_replicates[i] = func(perm_sample_1, perm_sample_2) # any custom function
+
+    return perm_replicates
+	
+#################################################
+
+# Comparing the force push of two different frog types
+# Pipeline for hypothesis testing
+# 1- Clearly state the null hypothesis
+# 2- Define your test statistic
+# 3- Generate many sets of simulated data assuming the null hypothesis is true
+# 4- Compute the test statistic for each simulated data set
+# 5- The p-value is the fraction of your simulated data sets for
+	# which the test statistic is at least as extreme as for the real data
+
+# P-Value calculation
+def diff_of_means(data_1, data_2):
+    """Difference in means of two arrays."""
+
+    # The difference of means of data_1, data_2: diff
+    diff = np.mean(data_1)-np.mean(data_2)
+
+    return diff
+
+# Compute difference of mean impact force from experiment: empirical_diff_means.
+# H0: Would we get the difference of means 
+empirical_diff_means = diff_of_means(force_a, force_b)
+
+# Draw 10,000 permutation replicates: perm_replicates
+perm_replicates = draw_perm_reps(force_a, force_b, diff_of_means , size=10000)
+
+# Compute p-value: p
+p = np.sum(perm_replicates >= empirical_diff_means) / len(perm_replicates)
+
+# Print the result
+print('p-value =', p)
+
+# The p-value tells you that there is about a 0.6% chance that you would get the difference
+# of means observed in the experiment if frogs were exactly the same.
+# A p-value below 0.01 is typically said to be "statistically significant,"
+
+#################################################
+
